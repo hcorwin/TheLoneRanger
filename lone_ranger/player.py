eaa348f23a4cd835029b925d773b32c3a93e2339
hcorwin <43582690+hcorwin@users.noreply.github.com>
2020-02-23 17:58:05 -0500

    Update player.py
    full animation

--- lone_ranger/player.py
+++ lone_ranger/player.py
@@ -8,10 +8,17 @@ class Player(Character):
     moving, throwing/shooting, collisions, etc.  It was hastily
     written as a demo but should direction.
     """
+
     def __init__(self, z=0, x=0, y=0):
         super().__init__(z, x, y)
         # This unit's health
         self.health = 100
+        # Length of time that can be sprinted
+        self.sprintTime = 25
+        # Time since last sprint
+        self.sprintCooldown = 10
+        # Can or cannot sprint
+        self.canSprint = True
         # Last time I was hit
         self.last_hit = pygame.time.get_ticks()
         # A unit-less value.  Bigger is faster.
@@ -19,11 +26,6 @@ class Player(Character):
         # Where the player is positioned
         self.x = x
         self.y = y
-        # The image to use.  This will change frequently
-        # in an animated Player class.
-        self.image = pygame.image.load('./assets/ranger_right.png').convert_alpha()
-        self.image = pygame.transform.scale(self.image, (64, 64))
-        self.rect = self.image.get_rect()
         # How big the world is, so we can check for boundries
         self.world_size = (Settings.width, Settings.height)
         # What sprites am I not allowd to cross?
@@ -31,6 +33,16 @@ class Player(Character):
         # Which collision detection function?
         self.collide_function = pygame.sprite.collide_circle
         self.collisions = []
+        self.lives = 4
+        #Having to do with animation
+        self.step_count = 0
+        self.images = {}
+        self.direction = Direction.EAST
+        self.state = State.IDLE
+        self.state_time = pygame.time.get_ticks()
+        self.images[State.MOVE] = self.load_images("./assets/run/survivor-move_rifle_0")
+        self.image = self.images[State.MOVE][Direction.EAST][0]
+        self.frame = 0
         # For collision detection, we need to compare our sprite
         # with collideable sprites.  However, we have to remap
         # the collideable sprites coordinates since they change.
@@ -41,97 +53,232 @@ class Player(Character):
         self.collider.image = pygame.Surface([Settings.tile_size, Settings.tile_size])
         self.collider.rect = self.collider.image.get_rect()
         # Overlay
-        self.font = pygame.font.Font('freesansbold.ttf',32)
-        self.overlay = self.font.render(str(self.health) + "        4 lives", True, (0,0,0))
+        self.font = pygame.font.Font('freesansbold.ttf', 32)
+        self.overlay = self.font.render(str(self.health) + "        " + str(self.lives), True, (0, 0, 0))
 
-    def move_left(self, time):
-    
-        self.image = pygame.image.load('./assets/ranger_left.png').convert_alpha()
-        self.image = pygame.transform.scale(self.image, (64, 64))
-    
-        amount = self.delta * time
-        try:
-            if self.x - amount < 0:
-                raise OffScreenLeftException
-            else:
-                self.x = self.x - amount
-                self.update(0)
-                while(len(self.collisions) != 0):
-                    self.x = self.x + amount
-                    self.update(0)
-        except:
-            pass
+        self.direction = 2
 
-    def move_right(self, time):
-    
-        self.image = pygame.image.load('./assets/ranger_right.png').convert_alpha()
-        self.image = pygame.transform.scale(self.image, (64, 64))
-    
-        self.collisions = []
-        amount = self.delta * time
-        try:
-            if self.x + amount > self.world_size[0] - Settings.tile_size:
-                raise OffScreenRightException
+        # Wave the player is on
+        self.wave = 1
+        # number of zomibes spawned in current wave
+        self.zombieCount = 0
+    def update_image(self):
+        if self.state != State.MOVE:
+            self.image = self.images[self.state][self.direction][self.frame % 10]
+        else:
+            self.image = self.images[self.state][self.direction][self.step_count]
+
+    def updateSprite(self, keys):
+        if keys[pygame.K_d]:
+            if keys[pygame.K_w]:
+                self.switch_state(State.MOVE)
+                if self.direction != Direction.NORTHEAST:
+                    self.switch_dir(Direction.NORTHEAST)
+                else:
+                    self.step_count = (self.step_count + 1) % 10
+                self.update_image()
+            elif keys[pygame.K_s]:
+                self.switch_state(State.MOVE)
+                if self.direction != Direction.SOUTHEAST:
+                    self.switch_dir(Direction.SOUTHEAST)
+                else:
+                    self.step_count = (self.step_count + 1) % 10
+                self.update_image()
             else:
-                self.x = self.x + amount
-                self.update(0)
-                while(len(self.collisions) != 0):
+                self.switch_state(State.MOVE)
+                if self.direction != Direction.EAST:
+                    self.switch_dir(Direction.EAST)
+                else:
+                    self.step_count = (self.step_count + 1) % 10
+                self.update_image()
+
+        elif keys[pygame.K_a]:
+            if keys[pygame.K_w]:
+                self.switch_state(State.MOVE)
+                if self.direction != Direction.NORTHWEST:
+                    self.switch_dir(Direction.NORTHWEST)
+                else:
+                    self.step_count = (self.step_count + 1) % 10
+                self.update_image()
+
+            elif keys[pygame.K_s]:
+                self.switch_state(State.MOVE)
+                if self.direction != Direction.SOUTHWEST:
+                    self.switch_dir(Direction.SOUTHWEST)
+                else:
+                    self.step_count = (self.step_count + 1) % 10
+                self.update_image()
+
+            else:
+                self.switch_state(State.MOVE)
+                if self.direction != Direction.WEST:
+                    self.switch_dir(Direction.WEST)
+                else:
+                    self.step_count = (self.step_count + 1) % 10
+                self.update_image()
+
+        elif keys[pygame.K_w]:
+            self.switch_state(State.MOVE)
+            if self.direction != Direction.NORTH:
+                self.switch_dir(Direction.NORTH)
+            else:
+                self.step_count = (self.step_count + 1) % 10
+            self.update_image()
+        elif keys[pygame.K_s]:
+            self.switch_state(State.MOVE)
+            if self.direction != Direction.SOUTH:
+                self.switch_dir(Direction.SOUTH)
+            else:
+                self.step_count = (self.step_count + 1) % 10
+            self.update_image()
+
+    def move(self, time, keys):
+
+        amount = self.delta * time
+
+        if keys[pygame.K_LSHIFT] and self.sprintTime > 0 and self.canSprint:
+            amount = amount * 1.5
+            self.sprintTime = self.sprintTime - 1
+            if self.sprintCooldown > 0:
+                self.sprintCooldown = self.sprintCooldown - 1
+
+        if self.sprintTime < 25 and self.sprintCooldown == 10:
+
+            self.sprintTime = self.sprintTime + 1
+            if self.sprintTime > 10:
+                self.canSprint = True
+
+        if self.sprintCooldown < 10:
+            if self.sprintCooldown == 0 and self.sprintTime == 0 and self.canSprint:
+                self.canSprint = False
+            elif self.canSprint == False:
+                self.sprintCooldown = self.sprintCooldown + 1
+
+        if (keys[pygame.K_a] and keys[pygame.K_s]) or (keys[pygame.K_a] and keys[pygame.K_w]):
+            amount = amount / 2
+
+        if (keys[pygame.K_d] and keys[pygame.K_s]) or (keys[pygame.K_d] and keys[pygame.K_w]):
+            amount = amount / 2
+
+        if keys[pygame.K_a]:
+            try:
+                if self.x - amount < 0:
+                    raise OffScreenLeftException
+                else:
                     self.x = self.x - amount
                     self.update(0)
-        except:
-            pass
+                    while (len(self.collisions) != 0):
+                        self.x = self.x + amount
+                        self.update(0)
+            except:
+                pass
 
-    def move_up(self, time):
-    
-        self.image = pygame.image.load('./assets/ranger_up.png').convert_alpha()
-        self.image = pygame.transform.scale(self.image, (64, 64))
-    
-        self.collisions = []
-        amount = self.delta * time
-        try:
-            if self.y - amount < 0:
-                raise OffScreenTopException
-            else:
-                self.y = self.y - amount
-                self.update(0)
-                if len(self.collisions) != 0:
-                    self.y = self.y + amount
+        if keys[pygame.K_d]:
+            try:
+                if self.x + amount > self.world_size[0] - Settings.tile_size:
+                    raise OffScreenRightException
+                else:
+                    self.x = self.x + amount
                     self.update(0)
-                    self.collisions = []
-        except:
-            pass
+                    while (len(self.collisions) != 0):
+                        self.x = self.x - amount
+                        self.update(0)
+            except:
+                pass
 
-    def move_down(self, time):
-    
-        self.image = pygame.image.load('./assets/ranger_down.png').convert_alpha()
-        self.image = pygame.transform.scale(self.image, (64, 64))
-    
-        amount = self.delta * time
-        try:
-            if self.y + amount > self.world_size[1] - Settings.tile_size:
-                raise OffScreenBottomException
-            else:
-                self.y = self.y + amount
-                self.update(0)
-                if len(self.collisions) != 0:
+        if keys[pygame.K_w]:
+            try:
+                if self.y - amount < 0:
+                    raise OffScreenTopException
+                else:
                     self.y = self.y - amount
                     self.update(0)
+                    if len(self.collisions) != 0:
+                        self.y = self.y + amount
+                        self.update(0)
+                        self.collisions = []
+            except:
+                pass
+
+        if keys[pygame.K_s]:
+            try:
+                if self.y + amount > self.world_size[1] - Settings.tile_size:
+                    raise OffScreenBottomException
+                else:
+                    self.y = self.y + amount
+                    self.update(0)
+                    if len(self.collisions) != 0:
+                        self.y = self.y - amount
+                        self.update(0)
                     self.collisions = []
-        except:
-            pass
+            except:
+                pass
+
+        self.updateSprite(keys)
 
     def update(self, time):
         self.rect.x = self.x
         self.rect.y = self.y
         self.collisions = []
+
+        self.frame = (self.frame + 1) % Settings.fps
+        self.image = self.images[self.state][self.direction][self.frame % 10]
+
         for sprite in self.blocks:
-            self.collider.rect.x= sprite.x
+            self.collider.rect.x = sprite.x
             self.collider.rect.y = sprite.y
             if pygame.sprite.collide_rect(self, self.collider):
                 self.collisions.append(sprite)
+
+    def switch_state(self, state):
+        time = pygame.time.get_ticks()
+        if self.state != state:
+            self.state = state
+            self.state_time = time
+
+    def switch_dir(self, direction):
+        self.direction = direction
+        self.step_count = 0
+
+    def load_images(self, path):
+        images = {}
+        right = {}
+        left = {}
+        up = {}
+        down = {}
+        upright = {}
+        upleft = {}
+        downleft = {}
+        downright = {}
+
+        for i in range(0, 20):
+            p = path + str(i) + ".png"
+            temp = pygame.image.load(p).convert_alpha()
+            temp = pygame.transform.scale(temp, (64, 64))
+            right[i] = temp
+            left[i] = pygame.transform.flip(temp, True, False)
+            up[i] = pygame.transform.rotate(temp, 90)
+            down[i] = pygame.transform.rotate(temp, 270)
+            upleft[i] = pygame.transform.rotate(temp, 135)
+            upright[i] = pygame.transform.rotate(temp, 45)
+            downleft[i] = pygame.transform.rotate(temp, 225)
+            downright[i] = pygame.transform.rotate(temp, 315)
+        images[Direction.EAST] = right
+        images[Direction.WEST] = left
+        images[Direction.NORTH] = up
+        images[Direction.SOUTH] = down
+        images[Direction.NORTHEAST] = upright
+        images[Direction.NORTHWEST] = upleft
+        images[Direction.SOUTHWEST] = downleft
+        images[Direction.SOUTHEAST] = downright
+        return images
+
 
     def ouch(self):
         now = pygame.time.get_ticks()
         if now - self.last_hit > 1000:
             self.health = self.health - 10
             self.last_hit = now
+        if self.health <= 0:
+            self.health = 100
+            self.lives = self.lives - 1
