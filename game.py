eb56b62eeade89fdceaece701cf5ea8a685ce61d
hcorwin <43582690+hcorwin@users.noreply.github.com>
2020-02-24 10:58:23 -0500

    Update game.py

--- game.py
+++ game.py
@@ -2,6 +2,7 @@
 
 import pygame
 import sys
+
 sys.path.append('..')
 import league
 from player import Player
@@ -16,33 +17,33 @@ a Game class that organizes this code better (and is
 reusable).
 """
 
+
 # Function to call when colliding with zombie
 
 def main():
-    
     e = league.Engine("The Lone Ranger")
     e.init_pygame()
 
     sprites = league.Spritesheet('./assets/base_chip_pipo.png', league.Settings.tile_size, 8)
-    t = league.Tilemap('./assets/world.lvl', sprites, layer = 1)
-    b = league.Tilemap('./assets/background.lvl', sprites, layer = 0)
+    t = league.Tilemap('./assets/world.lvl', sprites, layer=1)
+    b = league.Tilemap('./assets/background.lvl', sprites, layer=0)
     world_size = (t.wide * league.Settings.tile_size, t.high * league.Settings.tile_size)
-    
+
     e.drawables.add(t.passable.sprites())
     e.drawables.add(b.passable.sprites())
-    
+
     p = Player(2, 400, 300)
     o = Overlay(p)
     p.blocks.add(t.impassable)
     p.world_size = world_size
     p.rect = p.image.get_rect()
-    
+
     e.drawables.add(p)
-    
+
     e.drawables.add(o)
-    
+
     c = league.LessDumbCamera(800, 600, p, e.drawables, world_size)
-    
+
     e.objects.append(c)
     e.objects.append(o)
 
@@ -53,63 +54,57 @@ def main():
     pygame.mixer.music.load('./assets/ANewJourney.ogg')
     pygame.mixer.music.play(-1)
     pygame.mixer.music.set_volume(0.3)
-    
-    wave = 1
-    zombieCount = 0
-    
-    
-    #Register events?
-    pygame.time.set_timer(pygame.USEREVENT + 0, 250// league.Settings.gameTimeFactor)
+
+    # Register events?
+    pygame.time.set_timer(pygame.USEREVENT + 0, 10 // league.Settings.gameTimeFactor)
     pygame.time.set_timer(pygame.USEREVENT + 1, 250 // league.Settings.gameTimeFactor)
- 
-    #Player movements
 
+    # Player movements
 
-    
     zombies = []
-
-    
 
     def shoot(time):
         now = pygame.time.get_ticks()
         if now - p.lastShot > 500:
             shot_sound.play()
-            bullet = Bullet(p.direction,10, p.x, p.y)
+            bullet = Bullet(p.direction, 10, p.x, p.y)
             e.objects.append(bullet)
             e.drawables.add(bullet)
-     
-            
+
             for z in zombies:
-                e.collisions[z] = (bullet,z.ouch)
-           
+                e.collisions[z] = (bullet, z.ouch)
 
             p.lastShot = now
-    
-   
+            e.objects.remove(c)
+            e.objects.append(c)
+
     '''
     Spawns Zombies with a 25% freqency every 250ms
     '''
+
     def spwanZombies(time):
-        listx = [0, 0, 400, 800]
-        listy = [0, 300, 0, 300]
-        randomInt = random.randint(0, 3)
-        if p.zombieCount < p.wave * 5:
-            if random.randint(1, 100) > 75:
-                z = Zombie(p, zombie_sound, 10, listx[randomInt], listy[randomInt])
-                e.drawables.add(z)
-                e.objects.append(z)     
-                e.collisions[z] = (p, p.ouch)
-                p.zombieCount = p.zombieCount +1
-                zombies.append(z)
-                zombie_sound.play()
-                
-                
-            
+        if p.zombieCount <= p.wave * 5:
+            listx = [0, 0, 400, 1000]
+            listy = [0, 300, 0, 300]
+            randomInt = random.randint(0, 3)
+            if p.zombieCount < p.wave * 5:
+                if random.randint(1, 100) > 75:
+                    z = Zombie(p, 10, listx[randomInt], listy[randomInt])
+                    e.drawables.add(z)
+                    e.objects.append(z)
+                    e.collisions[z] = (p, p.ouch)
+                    p.zombieCount = p.zombieCount + 1
+                    zombies.append(z)
+                    e.objects.remove(c)
+                    e.objects.append(c)
+                    if random.randint(1, 100) > 75:
+                        zombie_sound.play()
+
     def moveAndShoot(time):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_w] or keys[pygame.K_a] or keys[pygame.K_s] or keys[pygame.K_d]  :
-            p.move(time ,pygame.key.get_pressed())
-        if  keys[pygame.K_SPACE]:
+        if keys[pygame.K_w] or keys[pygame.K_a] or keys[pygame.K_s] or keys[pygame.K_d]:
+            p.move(time, pygame.key.get_pressed())
+        if keys[pygame.K_SPACE]:
             shoot(time)
 
     '''
@@ -118,39 +113,40 @@ def main():
 
     def updateZoms(time):
         for i in e.objects:
-            if isinstance(i,Zombie):
-                if random.randint(1,100) > 25:
-                    i.move_towards_player(time)
+            if isinstance(i, Zombie):
                 if i.health <= 0:
                     e.objects.remove(i)
                     e.drawables.remove(i)
-            if isinstance(i,Bullet):
-                if i.ttl > 10:
-                    i.kill()
-        e.objects.remove(c)
-        e.objects.append(c)
-           
+                    zombies.remove(i)
+                    p.kills += 1
+                    if p.zombieCount == p.wave * 5 and not zombies:
+                        p.wave += 1
+                        p.zombieCount = 0
+
+            if isinstance(i, Bullet):
+                if i.ttl > 250:
+                    e.objects.remove(i)
+                    e.drawables.remove(i)
+        if p.lives <= 0:
+            print(p.kills)
+            exit()
 
     e.key_events[pygame.K_a] = moveAndShoot
     e.key_events[pygame.K_d] = moveAndShoot
     e.key_events[pygame.K_w] = moveAndShoot
     e.key_events[pygame.K_s] = moveAndShoot
     e.key_events[pygame.K_SPACE] = moveAndShoot
-    
-    #Register zombie movements in event list
+
+    # Register zombie movements in event list
     e.events[pygame.USEREVENT] = updateZoms
-    e.events[pygame.USEREVENT + 1] =  spwanZombies
- 
-    
-    
-    #Sets up exit button to quit game
+    e.events[pygame.USEREVENT + 1] = spwanZombies
+
+    # Sets up exit button to quit game
     e.events[pygame.QUIT] = e.stop
-    
-    
-    #Runs the main game loop
+
+    # Runs the main game loop
     e.run()
 
-    
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
